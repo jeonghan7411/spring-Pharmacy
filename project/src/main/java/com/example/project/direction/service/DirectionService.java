ab6a4fab4a1cc55f4db70ae587562a33d0f1cf39
bangjeonghan <aufkrh@naver.com>
2024-08-31 15:31:29 +0900

    pharmacy의 dto를 따로 추가하여 entity->dto 로 변환하여 dto를 통해 활용 (entity는 데이터베이스와 밀접한 관련이 있어서) & 거리 계산 알고리즘 메서드 추가

---
+++ project/src/main/java/com/example/project/direction/service/DirectionService.java
@@ -0,0 +1,43 @@
+package com.example.project.direction.service;
+
+import com.example.project.api.dto.DocumentDto;
+import com.example.project.direction.entity.Direction;
+import com.example.project.pharmacy.dto.PharmacyDto;
+import com.example.project.pharmacy.service.PharmacySearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DirectionService {
+
+    private final PharmacySearchService pharmacySearchService;
+
+    public List<Direction> buildDirectionList(DocumentDto documentDto) {
+
+        // 약국 데이터 조회
+        List<PharmacyDto> pharmacyDtos = pharmacySearchService.searchPharmacyDtoList();
+
+        // 거리계산 알고리즘을 이용하여, 고객과 약국 사이의 거리를 계산하고 sort
+
+
+    }
+
+
+    // Haversine formula 알고리즘
+    // 고객의 위도,경도  /  약국의 위도,경도
+    // 고객과 약국사이의 거리를 구하는 알고리즘
+    private double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+        lat1 = Math.toRadians(lat1);
+        lon1 = Math.toRadians(lon1);
+        lat2 = Math.toRadians(lat2);
+        lon2 = Math.toRadians(lon2);
+
+        double earthRadius = 6371; //Kilometers
+        return earthRadius * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon1 - lon2));
+    }
+}
---
+++ project/src/main/java/com/example/project/pharmacy/service/PharmacySearchService.java
@@ -0,0 +1,45 @@
+package com.example.project.pharmacy.service;
+
+import com.example.project.pharmacy.dto.PharmacyDto;
+import com.example.project.pharmacy.entity.Pharmacy;
+import com.example.project.pharmacy.repository.PharmacyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacySearchService {
+
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+
+    public List<PharmacyDto> searchPharmacyDtoList() {
+
+        // redis
+
+
+        // db
+        return pharmacyRepositoryService.findAll()
+                .stream()
+                .map(this::convertToPharmacyDto)
+                .collect(Collectors.toList());
+    }
+
+
+    // entity는 데이터 베이스와 밀접한 연관이 있기 때문에 이것이 변경되는 것은 데이터베이스에 변경이 일어난다는 것이라 생각하여
+    // 프론트에게 전달이나 컨트롤러에 전달할때는 dto로 통신하는게 좋음
+    // 별도의 dto를 생성하여 컨버트 해주는 것
+    private PharmacyDto convertToPharmacyDto(Pharmacy pharmacy) {
+        return PharmacyDto.builder()
+                .id(pharmacy.getId())
+                .pharmacyAddress(pharmacy.getPharmacyAddress())
+                .pharmacyName(pharmacy.getPharmacyName())
+                .latitude(pharmacy.getLatitude())
+                .longitude(pharmacy.getLongitude())
+                .build();
+    }
+}
