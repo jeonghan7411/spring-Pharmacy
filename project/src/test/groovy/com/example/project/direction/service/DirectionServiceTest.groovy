f0ec16f1a767e07723f3f24c093bfcc13866a51b
bangjeonghan <aufkrh@naver.com>
2024-08-31 16:20:06 +0900

    거리 계산 알고리즘 테스트코드 작성

---
+++ project/src/test/groovy/com/example/project/direction/service/DirectionServiceTest.groovy
@@ -0,0 +1,96 @@
+package com.example.project.direction.service
+
+import com.example.project.api.dto.DocumentDto
+import com.example.project.direction.entity.Direction
+import com.example.project.pharmacy.dto.PharmacyDto
+import com.example.project.pharmacy.service.PharmacySearchService
+import spock.lang.Specification
+
+// 여기서는 작은 단위 테스트로 진행
+
+class DirectionServiceTest extends Specification {
+
+    private PharmacySearchService pharmacySearchService = Mock() //여기서 단위테스트이기때문에 스프링컨테이너가 띄워져있지않음 그래서 Mock객체로 만들어야함
+
+    private DirectionService directionService = new DirectionService(pharmacySearchService)
+
+    private List<PharmacyDto> pharmacyList
+    // 그래서 여기서 이렇게 만들어줌
+    // 아래에서 사용하는곳에서 지정해줘야함 54번째줄
+    def setup() {
+        pharmacyList = new ArrayList<>()
+        pharmacyList.addAll(
+                PharmacyDto.builder()
+                        .id(1L)
+                        .pharmacyName("돌곶이온누리약국")
+                        .pharmacyAddress("주소1")
+                        .latitude(37.61040424)
+                        .longitude(127.0569046)
+                        .build(),
+                PharmacyDto.builder()
+                        .id(2L)
+                        .pharmacyName("호수온누리약국")
+                        .pharmacyAddress("주소2")
+                        .latitude(37.60894036)
+                        .longitude(127.029052)
+                        .build()
+        )
+    }
+
+    def "buildDirectionList - 결과 값이 거리 순으로 정렬이 되는지 확인"() {
+        given:
+        def addressName = "서울 성북구 종암로10길"
+        double inputLatitude = 37.5960650456809
+        double inputLongitude = 127.037033003036
+
+        def documentDto = DocumentDto.builder()
+                .addressName(addressName)
+                .latitude(inputLatitude)
+                .longitude(inputLongitude)
+                .build()
+
+        when:
+
+        pharmacySearchService.searchPharmacyDtoList() >> pharmacyList // 만들어놓은것을 보도록 지정해준것
+
+        def results = directionService.buildDirectionList(documentDto)
+
+        then:
+        results.size() == 2
+        results.get(0).targetPharmacyName == "호수온누리약국"
+        results.get(1).targetPharmacyName == "돌곶이온누리약국"
+    }
+
+    def "buildDirectionList - 정해진 반경 10km 내에 검색이 되는지 확인"() {
+        given:
+        pharmacyList.add(
+                PharmacyDto.builder()
+                        .id(3L)
+                        .pharmacyName("경기약국")
+                        .pharmacyAddress("주소3")
+                        .latitude(37.3825107393401)
+                        .longitude(127.236707811313)
+                        .build())
+
+        def addressName = "서울 성북구 종암로10길"
+        double inputLatitude = 37.5960650456809
+        double inputLongitude = 127.037033003036
+
+        def documentDto = DocumentDto.builder()
+                .addressName(addressName)
+                .latitude(inputLatitude)
+                .longitude(inputLongitude)
+                .build()
+
+        when:
+        pharmacySearchService.searchPharmacyDtoList() >> pharmacyList
+
+        def results = directionService.buildDirectionList(documentDto)
+
+        then:
+        results.size() == 2
+        results.get(0).targetPharmacyName == "호수온누리약국"
+        results.get(1).targetPharmacyName == "돌곶이온누리약국"
+
+    }
+}
