9392f8ccbe957107734ffe5a511cd2f6bcddb746
bangjeonghan <aufkrh@naver.com>
2024-09-08 23:48:14 +0900

    handlebars를 사용한 view단, 및 MockMVC 활용한 컨트롤러 테스트

--- project/src/main/java/com/example/project/pharmacy/service/PharmacyRecommendationService.java
+++ project/src/main/java/com/example/project/pharmacy/service/PharmacyRecommendationService.java
@@ -3,6 +3,7 @@ package com.example.project.pharmacy.service;
 import com.example.project.api.dto.DocumentDto;
 import com.example.project.api.dto.KakaoApiResponseDto;
 import com.example.project.api.service.KakaoAddressSearchService;
+import com.example.project.direction.dto.OutputDto;
 import com.example.project.direction.entity.Direction;
 import com.example.project.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,23 +24,38 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendPharmacyList(String address) {
+    public List<OutputDto> recommendPharmacyList(String address) {
 
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
         // 널 값이 거나 Document 부분이 비어 있다면
         if(Objects.isNull(kakaoApiResponseDto) || CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
             log.error("[PharmacyRecommendationService recommendPharmacyList] Input address: {}", address);
-            return;
+            return Collections.emptyList();
         }
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
 
-        // 공공 데이터 활용
-//        List<Direction> directionList = directionService.buildDirectionList(documentDto);
-        //
-        List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
+        //  공공기관 약국 데이터 및  거리계산 알고리즘 이용
+        List<Direction> directionList = directionService.buildDirectionList(documentDto);
 
-        directionService.saveAll(directionList);
+        //  kakao 카테고리를 이요한 장소 검색 api
+//        List<Direction> directionList = directionService.buildDirectionListByCategoryApi(documentDto);
+
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(t -> convertToOutputDto(t))
+                .collect(Collectors.toList());
+    }
+
+    // entity는 데이터베이스와 밀접한 관계가 있기 때문에 controller단에서 사용할 OutputDto로 컨버팅 해주는 메서드
+    private OutputDto convertToOutputDto(Direction direction) {
+        return OutputDto.builder()
+                .pharmacyName(direction.getTargetPharmacyName())
+                .pharmacyAddress(direction.getTargetAddress())
+                .directionUrl("todo")
+                .roadViewUrl("todo")
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
 }
